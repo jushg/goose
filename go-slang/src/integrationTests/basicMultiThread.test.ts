d8c4b1aa6c19ccf6d91ae686e89a392eb0f50611
Bharath Sudheer <bharath.sudheer@gmail.com>
2024-04-08 18:18:09 +0800

    Complete tests for mutex

--- go-slang/src/integrationTests/basicMultiThread.test.ts
+++ go-slang/src/integrationTests/basicMultiThread.test.ts
@@ -2,7 +2,41 @@ import { compileParsedProgram } from "../compiler";
 import { parse } from "../parser";
 import { executeStep, initializeVirtualMachine } from "../virtual_machine";
 
-const progStr = `
+const setUpMultiThreadedTest = (progStr: string) => {
+  const log: Record<string, string[]> = {};
+  const prog = compileParsedProgram(parse(progStr));
+  let state = initializeVirtualMachine(prog, (2 ** 8) ** 2, (ctx, s) => {
+    const pushLog = (id: string, s: string) => {
+      if (!log[id]) log[id] = [];
+      log[id].push(s);
+    };
+    "threadId" in ctx ? pushLog(ctx.threadId, s) : pushLog(ctx.component, s);
+  });
+
+  const getId = () => state.jobState.getId();
+  const getMemory = () => state.machineState.HEAP;
+  const getThread = () => state.jobState;
+  const getPC = () => state.jobState.getPC().addr;
+  const _getRts = () =>
+    getThread()
+      .getRTS()
+      .getScopeData()
+      .map(({ env }) => env);
+
+  return {
+    log,
+    prog,
+    state,
+    getId,
+    getMemory,
+    getThread,
+    getPC,
+    _getRts,
+  };
+};
+
+describe("basic multi threaded program", () => {
+  const progStr = `
 var x int
 
 func main() {
@@ -35,55 +69,38 @@ func foo(y int) {
 }
 `;
 
-describe("basic multi threaded program", () => {
   it("should execute correctly", () => {
-    const log: Record<string, string[]> = {};
-    const prog = compileParsedProgram(parse(progStr));
-    let state = initializeVirtualMachine(prog, (2 ** 8) ** 2, (ctx, s) => {
-      const pushLog = (id: string, s: string) => {
-        if (!log[id]) log[id] = [];
-        log[id].push(s);
-      };
-      "threadId" in ctx ? pushLog(ctx.threadId, s) : pushLog(ctx.component, s);
-    });
-
-    const getId = () => state.jobState.getId();
-    const getMemory = () => state.machineState.HEAP;
-    const getThread = () => state.jobState;
-    const getPC = () => state.jobState.getPC().addr;
-    const _getRts = () =>
-      getThread()
-        .getRTS()
-        .getScopeData()
-        .map(({ env }) => env);
-
-    const pcExecutionOrder: Record<string, number[]> = {};
+    let { log, state, getId, getPC, prog } = setUpMultiThreadedTest(progStr);
     const maxInstrExecutions = 10000;
-    let lastHundredInstr: any[] = [];
-    const updateLastHundredInstr = () => {
-      lastHundredInstr = pcExecutionOrder[getId()]
-        .slice(-100)
+    const pcExecutionOrder: Record<string, number[]> = {};
+    const printLast = (id: string, num: number, detailedNum: number = 10) => {
+      let lastInstr: any[] = [];
+      lastInstr = (pcExecutionOrder[id] ?? [])
+        .slice(-num)
         .map((i) => [i, prog.instructions[i].op]);
-      lastHundredInstr = lastHundredInstr.map(([i, op], idx) => {
-        if (idx < lastHundredInstr.length - 10) return { i, op };
+      lastInstr = lastInstr.map(([i, op], idx) => {
+        if (idx < lastInstr.length - detailedNum) return { i, op };
         return { i, ...prog.instructions[i] };
       });
+      return lastInstr;
     };
-
-    while (true) {
+    const takeNoteOfLatestPC = () => {
       if (!pcExecutionOrder[getId()]) pcExecutionOrder[getId()] = [];
       pcExecutionOrder[getId()].push(getPC());
-      updateLastHundredInstr();
 
       if (pcExecutionOrder[getId()].length > maxInstrExecutions)
         expect(pcExecutionOrder[getId()]).toHaveLength(0);
+    };
+
+    while (true) {
+      takeNoteOfLatestPC();
 
       try {
         const newState = executeStep(state);
         if (newState === null) break;
         state = newState;
       } catch (e) {
-        console.dir(lastHundredInstr);
+        console.dir(printLast(getId(), 100, 20));
         throw e;
       }
 
@@ -95,7 +112,7 @@ describe("basic multi threaded program", () => {
     // console.dir(lastHundredInstr);
     expect(Object.keys(log)).toHaveLength(1 + /* getFn */ 100 + /* foo */ 1);
 
-    const mainId = "t_00001__from_____0";
+    const mainId = state.machineState.MAIN_ID;
     const otherIdsAndPc = Object.keys(log)
       .filter((id) => id !== mainId)
       .map((id) => {
@@ -152,5 +169,115 @@ describe("basic multi threaded program", () => {
 });
 
 describe("Test concurrency primitives", () => {
-  it.todo("test_and_set instructions");
+  it("test mutex", () => {
+    const progStr = `
+var x int
+var m *int
+
+func main() {
+  m = mutexInit()
+  bar := func () {
+    mutexLock(m)
+    print(x)
+    mutexUnlock(m)
+  }
+
+
+  mutexLock(m)
+
+  for i := 0; i < 5; i = i + 1 { go bar() }
+
+  print(x)
+  go foo(1)
+
+  for i := 0; i < 5; i = i + 1 { go bar() }
+
+  mutexUnlock(m)
+}
+
+func foo(y int) {
+  x = 100
+  print(x)
+}
+    `;
+    let { log, state, getId, prog, getPC } = setUpMultiThreadedTest(progStr);
+    const maxInstrExecutions = 10000;
+    const pcExecutionOrder: Record<string, number[]> = {};
+    const printLast = (id: string, num: number, detailedNum: number = 10) => {
+      let lastInstr: any[] = [];
+      lastInstr = (pcExecutionOrder[id] ?? [])
+        .slice(-num)
+        .map((i) => [i, prog.instructions[i].op]);
+      lastInstr = lastInstr.map(([i, op], idx) => {
+        if (idx < lastInstr.length - detailedNum) return { i, op };
+        return { i, ...prog.instructions[i] };
+      });
+      return lastInstr;
+    };
+    const takeNoteOfLatestPC = () => {
+      if (!pcExecutionOrder[getId()]) pcExecutionOrder[getId()] = [];
+      pcExecutionOrder[getId()].push(getPC());
+
+      if (pcExecutionOrder[getId()].length > maxInstrExecutions)
+        expect(pcExecutionOrder[getId()]).toHaveLength(0);
+    };
+
+    const ls: string[] = [];
+
+    while (true) {
+      ls.push(getId());
+      takeNoteOfLatestPC();
+
+      try {
+        const newState = executeStep(state);
+        if (newState === null) break;
+        state = newState;
+      } catch (e) {
+        // console.dir(printLast(getId(), 100, 20));
+        throw e;
+      }
+
+      // const _memUsage = `${getMemory().getMemoryUsed()} / ${getMemory().getMemorySize()}`;
+      // const _memResidency = `${getMemory().getMemoryResidency()} / ${getMemory().getMemorySize()}`;
+      // console.dir({ i: pcExecutionOrder.length, _memUsage, _memResidency });
+    }
+
+    const mainId = state.machineState.MAIN_ID;
+    // console.dir(printLast(mainId, 100, 20));
+
+    expect(Object.keys(log)).toHaveLength(1 + /* getFn */ 10 + /* foo */ 1);
+
+    const otherIdsAndPc = Object.keys(log)
+      .filter((id) => id !== mainId)
+      .map((id) => {
+        const idSplit = id.split("_");
+        return [parseInt(idSplit.at(-1)!, 16), id] as const;
+      })
+      .reduce((acc: Record<number, string[]>, [pc, id]) => {
+        if (acc[pc] === undefined) acc[pc] = [];
+        acc[pc].push(id);
+        return acc;
+      }, {});
+    const printXIds: string[] = Object.entries(otherIdsAndPc)
+      .filter(([_, ids]) => ids.length === 10)
+      .flatMap(([_, ids]) => ids);
+    const fooId = Object.entries(otherIdsAndPc).filter(
+      ([_, ids]) => ids.length === 1
+    )![0][1][0];
+
+    expect(Object.keys(log)).toContain(mainId);
+    expect(log[mainId]).toHaveLength(1);
+    expect(log[mainId]).toContain("0");
+
+    expect(log[fooId]).toHaveLength(1);
+    expect(log[fooId]).toContain("100");
+
+    expect(printXIds).toBeDefined();
+    for (const printXId of printXIds) {
+      expect(log[printXId]).not.toContain("'This is main.'");
+      expect(log[printXId]).toHaveLength(1);
+      const [x] = log[printXId];
+      expect(parseInt(x)).toEqual(100);
+    }
+  });
 });
