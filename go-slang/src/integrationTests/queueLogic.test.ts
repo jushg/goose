ed86b0fb738497c49cf7f4e1c7a561cc89b5aec6
Trong Tan <e0550425@u.nus.edu>
2024-04-13 15:05:42 +0800

    refactor utils

---
+++ go-slang/src/integrationTests/queueLogic.test.ts
@@ -0,0 +1,50 @@
+import { executeStep } from "../virtual_machine";
+import { setUpTest } from "./utils";
+
+describe("Test queue logic", () => {
+  test.concurrent("test queue logic", async () => {
+    const progStr = `
+func main() {
+  q := queueInit(4)
+  print(queueCapacity(q))
+  print(getSizeTest(q))
+  print(queueSize(q))
+  print(queueEmpty(q))
+  print(queueFull(q))
+  pushBackQueue(q, 1)
+  pushBackQueue(q, 2)
+  pushBackQueue(q, 3)
+  pushBackQueue(q, 4)
+  print(queueFull(q))
+  print(popFrontQueue(q))
+  print(popFrontQueue(q))
+  print(popFrontQueue(q))
+  pushBackQueue(q, 5)
+  pushBackQueue(q, 6)
+  pushBackQueue(q, 7)
+  print(queueSize(q))
+  print(queueEmpty(q))
+  print(popFrontQueue(q))
+  print(popFrontQueue(q))
+  print(popFrontQueue(q))
+
+ 
+}
+`;
+
+    let { log, state, getId, prog, getPC } = setUpTest(progStr);
+    const maxInstrExecutions = 1000; // Reduced by 10x compared to other multi threaded test
+    const pcExecutionOrder: Record<string, number[]> = {};
+
+    while (true) {
+      try {
+        const newState = executeStep(state);
+        if (newState === null) break;
+        state = newState;
+      } catch (e) {
+        throw e;
+      }
+    }
+    console.dir(log);
+  });
+});
