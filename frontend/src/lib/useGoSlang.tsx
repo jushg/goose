0bf2eee4c61579e04c9f5c885f22c1c9e7b18f5f
Bharath Sudheer <bharath.sudheer@gmail.com>
2024-04-14 15:43:38 +0800

    Add clearer vm logging

--- frontend/src/lib/useGoSlang.tsx
+++ frontend/src/lib/useGoSlang.tsx
@@ -7,19 +7,19 @@ import {
   initializeVirtualMachine,
 } from "go-slang/src/virtual_machine";
 import { useCallback, useEffect, useState } from "react";
+import { useVmLogs } from "./useVmLog";
 
 export const useCompiler = () => {
   type CompilationState = "PARSE_FAILED" | "COMPILATION_FAILED" | "COMPILED";
   const [compiledFile, setCompiledFile] = useState<CompiledFile | null>(null);
   const [state, setState] = useState<CompilationState>("PARSE_FAILED");
 
-  const setGooseCode = (gooseCode: string) => {
+  const setGooseCode = async (gooseCode: string) => {
     console.info(`setting goose code: \n---\n${gooseCode}\n`);
 
     let parsedProg: ProgramObj;
     try {
       parsedProg = parse(gooseCode);
-      console.dir(parsedProg);
     } catch (e) {
       setState("PARSE_FAILED");
       console.error(e);
@@ -41,45 +41,31 @@ export const useCompiler = () => {
   return { setGooseCode, state, compiledFile };
 };
 
-export type LogItem = {
-  ctx: { threadId: string } | { component: string };
-  message: string;
-  timestamp: string;
-};
-
 export const useVm = (compiledFile: CompiledFile | null) => {
-  const [log, setLog] = useState<LogItem[]>([]);
+  const memorySizeInNodes = (2 ** 8) ** 2;
+  const { log, appendLog, resetLog } = useVmLogs();
   let vmState: ExecutionState | null = null;
 
   useEffect(() => {
-    console.log("initializing vm");
-
     if (!compiledFile) return;
+
+    console.info("initializing vm");
+
+    resetLog();
     const initState = initializeVirtualMachine(
       compiledFile,
-      (2 ** 8) ** 2,
-      (ctx, s) => {
-        const d = new Date();
-        setLog((log) => [
-          ...log,
-          {
-            ctx,
-            message: s,
-            timestamp: `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${d.getMilliseconds()}`,
-          },
-        ]);
-      }
+      memorySizeInNodes,
+      appendLog
     );
 
-    setLog([]);
     vmState = initState;
   }, [compiledFile]);
 
-  const executor = useCallback(() => {
+  const executor = useCallback(async (): Promise<boolean> => {
     if (!vmState) {
       return false;
     }
-    console.log(
+    console.info(
       `
     [ ] Executing step!!
       time left: ${vmState.machineState.TIME_SLICE}
@@ -89,12 +75,7 @@ export const useVm = (compiledFile: CompiledFile | null) => {
       )}`
     );
     const newVmState = executeStep(vmState);
-    if (!newVmState) {
-      console.log("no new vm state");
-      console.dir(vmState.jobState);
-      console.log(vmState.machineState.JOB_QUEUE.print());
-    }
-    console.log(`
+    console.info(`
     [x] ~~executed step~~
       time left: ${vmState.machineState.TIME_SLICE}
       thread: ${vmState.jobState.getId()}
