afc5d08ba9205050326399ae5e4c72987868984c
Bharath Sudheer <bharath.sudheer@gmail.com>
2024-04-14 13:50:43 +0800

    Use yarn link for FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { useCompiler, useVm } from "./lib/useGoSlang";
 
 function App() {
+  const { setGooseCode, compiledFile, state: compilerState } = useCompiler();
+  const { executeStep, log } = useVm(compiledFile);
+
+  useEffect(() => {
+    setGooseCode(`
+        func main() {
+          print("k");
+        }
+      `);
+  }, []);
+
+  useEffect(() => {
+    if (!compiledFile) return;
+    let maxInstr = 100_000;
+    while (maxInstr-- > 0) {
+      console.log("executing");
+      executeStep();
+    }
+  }, [compiledFile]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{compilerState}</p>
+        <br />
+        <p>Log Length: {log.length}</p>
+        <br />
+        Log:
+        <br />
+        {log.map((l) => (
+          <p>{JSON.stringify(l)}</p>
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
